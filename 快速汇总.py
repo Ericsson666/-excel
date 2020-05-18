8a1cc13e7aa8525fba343c3f5ac40ef0cb8a71ef
Ericsson666 <61679435+Ericsson666@users.noreply.github.com>
2020-05-18 22:22:27 +0800

    可以在任何文件夹使用

--- 快速汇总.py
+++ 快速汇总.py
@@ -4,18 +4,17 @@ import xlsxwriter as xlwt
 import os.path
 import os
 
-start_row = int(input('从第几行开始?>')
+start_row = int(input('从第几行开始?>'))
 file_address = input('文件夹的地址是?>')
-workbook = xlwt.Workbook('{}./汇总.xlsx'.format(file_address))
+workbook = xlwt.Workbook('{}\\汇总.xlsx'.format(file_address))
 worksheet = workbook.add_worksheet()
 n=0
-for i in os.listdir('{}./'.format(file_address)):
+for i in os.listdir('{}\\'.format(file_address)):
     if i.endswith('xlsx'):
-        file = xlrd.open_workbook(i)
+        file = xlrd.open_workbook(file_address+'\\'+i)
         info = file.sheet_by_index(0)
         for j in range(start_row-1,info.nrows):
             rows=info.row_values(j)
             worksheet.write_row(n,0,rows)
             n+=1
 workbook.close()
-                
